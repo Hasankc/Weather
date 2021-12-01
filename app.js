8ea6afa791d18fd3f491bb637f9bdcbeef3c67d5
HASAN <alhasanal_qaysi@yahoo.com>
2021-12-01 13:00:14 +0200

    Wroked

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
   
 
  
-    app.use(bodyParser.urlencoded({ extended: true }));
+    //app.use(bodyParser.urlencoded({ extended: true }));
 
   const app = express(); 
 
@@ -15,10 +15,10 @@
   });
     app.post("/", function(req, res){
     
-      const  query = req.body, cityName;
+       
       const appid = 'f9df0681f60a33fb0e12f66c79e2f007';
-      const query = req.body.name;
-      const unit = 'metric'
+      const query=req.body.cityName;
+      const unit = "metric";
       const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ query+ '&appid=' +appid +'&units=' + unit;
   https.get(url, function(response) {
     console.log(response.statusCode);
@@ -26,13 +26,13 @@
       
       response.on("data", function(data){
       const weatherData = JSON.parse(data);
-      const tamp = weatherData.list[0].main.temp;
+      const tamp = weatherData.main.temp;
       const weatherDes = weatherData.weather[0].Des;
       const icon = weatherData.weather[0].icon;
       const imgeURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
       res.write("The weather is" + weatherDes);
       res.write("<h1>the weather" + temp + "in"  + query + "here</h1>");
-      res.write(<img  src=" + imgeURL + " >");
+      res.write("<img  src=" + imgeURL + " >");
       res.send();
     });
 
@@ -43,5 +43,5 @@
 
 
 app.listen(3000, function(){
-  res.send("Listening on port 3000");
+ // res.send("Listening on port 3000");
 })
